b5783ae5e6a0c5611695a37448a18066e223228d
Bennett Daley <bennettdaley@gmail.com>
2019-04-22 15:46:22 -0500

    Added return

--- listener.py
+++ listener.py
@@ -27,7 +27,9 @@ def scanning(track_ids):
     #ser.write(b'2')
     #call image processing and get new locations for each track id
     #for num in track_locations:
-    #request.post('https://smart-turntable-webapp.herokuapp.com/api/', json={"track_id":track_id, "start":new_start, "end":new_end})
+    #request.post('https://smart-turntable-webapp.herokuapp.com/api/', 
+    #json={"track_id":track_id, "start":new_start, "end":new_end})
+    return 0
 
 def main():
     previous_status = ""
