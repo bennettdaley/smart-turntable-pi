f655084ea666cfc696cb086c676e5c921194fe91
Bennett Daley <bennettdaley@gmail.com>
2019-04-21 21:31:19 -0500

    Modified listener to include serial output

--- listener.py
+++ listener.py
@@ -1,27 +1,39 @@
 import requests
 import time
+import serial
+from serial import Serial
 
+ser = serial.Serial('/dev/ttyACM0', 115200)
+time.sleep(2)
 def playing_pass_track_to_arduino(track_data):
     track_data["playing_status"] = "playing"
+    ser.write(b'1')
     return track_data
 
 def paused_pass_track_to_arduino(track_data):
     track_data["playing_status"] = "paused"
+    ser.write(b'0')
     return track_data
 
 def main():
+    previous_status = ""
     while(True):
-        time.sleep(5)
+        time.sleep(1)
         playing_status = requests.get('https://smart-turntable-webapp.herokuapp.com/api/play_status')
         print(playing_status.json())
-        if playing_status.json()["play_status"] == "playing":
-            track_data = requests.get('https://smart-turntable-webapp.herokuapp.com/api/current_track')
-            print(track_data.json())
-            playing_pass_track_to_arduino(track_data.json())
-        elif playing_status.json()["play_status"] == "paused":
-            track_data = requests.get('https://smart-turntable-webapp.herokuapp.com/api/current_track')
-            print(track_data.json())
-            paused_pass_track_to_arduino(track_data.json())
+        if(previous_status != playing_status.json()["play_status"]):
+            if playing_status.json()["play_status"] == "playing":
+                track_data = requests.get('https://smart-turntable-webapp.herokuapp.com/api/current_track')
+                print(track_data.json())
+                previous_status = playing_status.json()["play_status"]
+                print(previous_status)
+                playing_pass_track_to_arduino(track_data.json())
+            elif playing_status.json()["play_status"] == "paused":
+                track_data = requests.get('https://smart-turntable-webapp.herokuapp.com/api/current_track')
+                print(track_data.json())
+                previous_status = playing_status.json()["play_status"]
+                print(previous_status)
+                paused_pass_track_to_arduino(track_data.json())
 
 
 if __name__ == "__main__":
